f9e809ca50ab71a26944cb47a2753227346beb06
Martin <martoo6@gmail.com>
2018-12-16 17:18:54 +0900

    Add some basic routes. Returns mock boards ids

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # m1nesweeper
+
+REST resources:
+
+GET `/board` -> Return all boards ids  
+POST `/board` -> Create new board  
+GET `/board/:ID` -> Returns board by id  
+POST `/board/:ID/click` payload: `{ x: 0, y: 0 }` -> Clicks on positions. Returns the new board.
--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -1,9 +1,18 @@
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.{HttpApp, Route}
 
-object WebServer extends HttpApp {
+object WebServer extends HttpApp with SprayJsonSupport {
+  import spray.json.DefaultJsonProtocol._
+
   lazy val routes: Route = path("healthcheck"){
     complete {
       "OK"
+    }
+  } ~ path("boards"){
+    get {
+      complete {
+        Array(1, 2, 3, 4)
+      }
     }
   }
 }
